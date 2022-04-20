d0c68e65d7eec0605e5911703921ab6a316a2534
Daedalus <ernestojosh2@gmail.com>
2022-04-20 13:25:24 +0800

    MVC architecture

---
+++ src/model/Product.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Product {
+    int productId;
+    String productName;
+    double productPrice;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [productId=" + productId + ", productName=" + productName + ", productPrice=" + productPrice
+                + "]";
+    }
+
+}
--- src/view/OrderMenu.java
+++ src/view/OrderMenu.java
@@ -1,3 +1,5 @@
+package view;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -41,15 +43,13 @@ import java.awt.Toolkit;
 public class OrderMenu extends JFrame {
 
 	private JPanel contentPane;
+
 	/**
 	 * Launch the application.
 	 */
-	
-	public static void main(String[] args) {
-		
 
-		
-		
+	public static void main(String[] args) {
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -61,17 +61,18 @@ public class OrderMenu extends JFrame {
 			}
 		});
 	}
+
 	private JTextField txt_addId;
 	private JTextField txt_removeId;
 	private JTable tbl_cart;
 	private JTable tbl_inventory;
-	
+
 	public OrderMenu() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\trist\\Documents\\_T.I.P\\2nd SEM\\MODULAR 2\\COMPROG\\final proj files\\icon.png"));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(
+				"C:\\Users\\trist\\Documents\\_T.I.P\\2nd SEM\\MODULAR 2\\COMPROG\\final proj files\\icon.png"));
 		setResizable(false);
 		setTitle("Ordering and Payment");
 		setFont(new Font("Arial Narrow", Font.PLAIN, 12));
-			
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 638, 665);
@@ -79,25 +80,25 @@ public class OrderMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JLabel lblTitle = new JLabel("Neo Products Computer Peripherals Shop");
 		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblTitle, BorderLayout.NORTH);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
-		
+
 		JPanel order_payment = new JPanel();
 		order_payment.setBorder(null);
 		tabbedPane.addTab("Ordering and Payment", null, order_payment, null);
 		GridBagLayout gbl_order_payment = new GridBagLayout();
-		gbl_order_payment.columnWidths = new int[]{0, 342, 39, 55, 61, 57, 0, 0};
-		gbl_order_payment.rowHeights = new int[]{0, 0, 31, 0, 0, 0, 0, 0};
-		gbl_order_payment.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_order_payment.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_order_payment.columnWidths = new int[] { 0, 342, 39, 55, 61, 57, 0, 0 };
+		gbl_order_payment.rowHeights = new int[] { 0, 0, 31, 0, 0, 0, 0, 0 };
+		gbl_order_payment.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_order_payment.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
 		order_payment.setLayout(gbl_order_payment);
-		
+
 		JLabel lblNewLabel_14 = new JLabel("Item List");
 		lblNewLabel_14.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_14.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -108,7 +109,7 @@ public class OrderMenu extends JFrame {
 		gbc_lblNewLabel_14.gridx = 1;
 		gbc_lblNewLabel_14.gridy = 0;
 		order_payment.add(lblNewLabel_14, gbc_lblNewLabel_14);
-		
+
 		JScrollPane pane_inventory = new JScrollPane();
 		GridBagConstraints gbc_pane_inventory = new GridBagConstraints();
 		gbc_pane_inventory.insets = new Insets(0, 0, 5, 5);
@@ -116,40 +117,42 @@ public class OrderMenu extends JFrame {
 		gbc_pane_inventory.gridx = 1;
 		gbc_pane_inventory.gridy = 1;
 		order_payment.add(pane_inventory, gbc_pane_inventory);
-		
+
 		tbl_inventory = new JTable();
 		tbl_inventory.getTableHeader().setReorderingAllowed(false);
 		tbl_inventory.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"ID", "Product Name", "Stock", "Price"
-			}
-		) {
+				new Object[][] {
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+				},
+				new String[] {
+						"ID", "Product Name", "Stock", "Price"
+				}) {
 			Class[] columnTypes = new Class[] {
-				Integer.class, String.class, Integer.class, Integer.class
+					Integer.class, String.class, Integer.class, Integer.class
 			};
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
+
 			boolean[] columnEditables = new boolean[] {
-				false, false, false, false
+					false, false, false, false
 			};
+
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
@@ -164,7 +167,7 @@ public class OrderMenu extends JFrame {
 		tbl_inventory.getColumnModel().getColumn(3).setResizable(false);
 		tbl_inventory.getColumnModel().getColumn(3).setMinWidth(60);
 		pane_inventory.setViewportView(tbl_inventory);
-		
+
 		JLabel lblNewLabel = new JLabel("Add Item(ID):");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.ABOVE_BASELINE_TRAILING;
@@ -172,7 +175,7 @@ public class OrderMenu extends JFrame {
 		gbc_lblNewLabel.gridx = 2;
 		gbc_lblNewLabel.gridy = 1;
 		order_payment.add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		txt_addId = new JTextField();
 		GridBagConstraints gbc_txt_addId = new GridBagConstraints();
 		gbc_txt_addId.insets = new Insets(0, 0, 5, 5);
@@ -181,7 +184,7 @@ public class OrderMenu extends JFrame {
 		gbc_txt_addId.gridy = 1;
 		order_payment.add(txt_addId, gbc_txt_addId);
 		txt_addId.setColumns(10);
-		
+
 		JSpinner spin_qty = new JSpinner();
 		spin_qty.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		GridBagConstraints gbc_spin_qty = new GridBagConstraints();
@@ -189,14 +192,14 @@ public class OrderMenu extends JFrame {
 		gbc_spin_qty.gridx = 4;
 		gbc_spin_qty.gridy = 1;
 		order_payment.add(spin_qty, gbc_spin_qty);
-		
+
 		JButton btnConfirmOrder = new JButton("Confirm");
 		GridBagConstraints gbc_btnConfirmOrder = new GridBagConstraints();
 		gbc_btnConfirmOrder.insets = new Insets(0, 0, 5, 5);
 		gbc_btnConfirmOrder.gridx = 5;
 		gbc_btnConfirmOrder.gridy = 1;
 		order_payment.add(btnConfirmOrder, gbc_btnConfirmOrder);
-		
+
 		JLabel lblNewLabel_11 = new JLabel("My Cart");
 		lblNewLabel_11.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_11.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -207,7 +210,7 @@ public class OrderMenu extends JFrame {
 		gbc_lblNewLabel_11.gridx = 1;
 		gbc_lblNewLabel_11.gridy = 3;
 		order_payment.add(lblNewLabel_11, gbc_lblNewLabel_11);
-		
+
 		JScrollPane pane_cart = new JScrollPane();
 		GridBagConstraints gbc_pane_cart = new GridBagConstraints();
 		gbc_pane_cart.insets = new Insets(0, 0, 5, 5);
@@ -215,39 +218,41 @@ public class OrderMenu extends JFrame {
 		gbc_pane_cart.gridx = 1;
 		gbc_pane_cart.gridy = 4;
 		order_payment.add(pane_cart, gbc_pane_cart);
-		
+
 		tbl_cart = new JTable();
 		tbl_cart.getTableHeader().setReorderingAllowed(false);
 		tbl_cart.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"ID", "Product Name", "Quantity", "Price"
-			}
-		) {
+				new Object[][] {
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+						{ null, null, null, null },
+				},
+				new String[] {
+						"ID", "Product Name", "Quantity", "Price"
+				}) {
 			Class[] columnTypes = new Class[] {
-				Integer.class, String.class, Integer.class, Integer.class
+					Integer.class, String.class, Integer.class, Integer.class
 			};
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
+
 			boolean[] columnEditables = new boolean[] {
-				false, false, false, false
+					false, false, false, false
 			};
+
 			public boolean isCellEditable(int row, int column) {
 				return columnEditables[column];
 			}
@@ -262,7 +267,7 @@ public class OrderMenu extends JFrame {
 		tbl_cart.getColumnModel().getColumn(3).setResizable(false);
 		tbl_cart.getColumnModel().getColumn(3).setMinWidth(60);
 		pane_cart.setViewportView(tbl_cart);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Remove Item(ID):");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
@@ -270,7 +275,7 @@ public class OrderMenu extends JFrame {
 		gbc_lblNewLabel_1.gridx = 2;
 		gbc_lblNewLabel_1.gridy = 4;
 		order_payment.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		txt_removeId = new JTextField();
 		GridBagConstraints gbc_txt_removeId = new GridBagConstraints();
 		gbc_txt_removeId.gridwidth = 2;
@@ -280,34 +285,31 @@ public class OrderMenu extends JFrame {
 		gbc_txt_removeId.gridy = 4;
 		order_payment.add(txt_removeId, gbc_txt_removeId);
 		txt_removeId.setColumns(10);
-		
+
 		JButton btnConfirmRemove = new JButton("Confirm");
 		GridBagConstraints gbc_btnConfirmRemove = new GridBagConstraints();
 		gbc_btnConfirmRemove.insets = new Insets(0, 0, 5, 5);
 		gbc_btnConfirmRemove.gridx = 5;
 		gbc_btnConfirmRemove.gridy = 4;
 		order_payment.add(btnConfirmRemove, gbc_btnConfirmRemove);
-		
+
 		JButton btnPromos = new JButton("Promos");
 		GridBagConstraints gbc_btnPromos = new GridBagConstraints();
 		gbc_btnPromos.insets = new Insets(0, 0, 5, 5);
 		gbc_btnPromos.gridx = 2;
 		gbc_btnPromos.gridy = 5;
 		order_payment.add(btnPromos, gbc_btnPromos);
-		
+
 		JButton btn_Payment = new JButton("Payment");
 		GridBagConstraints gbc_btn_Payment = new GridBagConstraints();
 		gbc_btn_Payment.insets = new Insets(0, 0, 5, 5);
 		gbc_btn_Payment.gridx = 5;
 		gbc_btn_Payment.gridy = 5;
 		order_payment.add(btn_Payment, gbc_btn_Payment);
-		
+
 		JPanel inventory = new JPanel();
 		tabbedPane.addTab("Inventory", null, inventory, null);
-		
-		
-		
-		
+
 	}
 
 }
